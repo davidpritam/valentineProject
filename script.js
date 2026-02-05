27eb02ce3385e0a4fa0ee3d328d314a1730e3e16
Priytam Kumar <pritamssm49@gmail.com>
2026-02-05 22:09:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const box = document.getElementById("btndiv")
+const nobtn = document.getElementById("btn2")
+const yesBtn = document.getElementById("btn1")
+const popup = document.getElementById("popup")
+const closedBtn = document.getElementById("closeBtn")
+
+// Show GIF popup when Yes button clicked
+yesBtn.addEventListener("click", () => {
+  popup.style.display = "flex";  // flex centers the GIF
+});
+
+// Close popup
+closeBtn.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+
+nobtn.addEventListener("mouseover",()=>{
+    console.log("button is clicked");
+    
+    const maxX = box.clientWidth - nobtn.offsetWidth;
+    const maxY = box.clientHeight - nobtn.offsetHeight;
+
+
+    const newX = Math.floor(Math.random() * maxX);
+    const newY = Math.floor(Math.random() * maxY);
+
+    nobtn.style.position = "absolute";
+    nobtn.style.left = newX + "px";
+    nobtn.style.top = newY + "px";
+
+})
